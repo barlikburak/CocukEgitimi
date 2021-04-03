631ed477483dbc7db6ad2349f0ae02a17b0d4fd1
barlikburak <barlikburak@hotmail.com>
2021-04-03 13:20:35 +0300

    arayuz yapildi

---
+++ CocukEgitimi/app/src/main/java/com/umay/cocukegitimi/MevsimActivity.java
@@ -0,0 +1,14 @@
+package com.umay.cocukegitimi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MevsimActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mevsim);
+    }
+}
